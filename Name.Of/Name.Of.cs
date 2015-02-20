73e0822d0db663f3ad319dd903fc6eb0927acdf6
Nick Strupat <nick.strupat@gmail.com>
2015-02-20 04:27:45 -0500

    shorter name

--- Name.Of/Name.Of.cs
+++ Name.Of/Name.Of.cs
@@ -5,19 +5,19 @@ public class R {}
 public static class Name {
 	static Name() {
 		// If this assembly has been loaded at run-time, the weaving failed to replace the calls to the place-holder methods below and should throw as soon as possible (i.e. this static constructor).
-		throw GetNotImplementedException();
+		throw GetException();
 	}
 
-    private static NotImplementedException GetNotImplementedException() { return new NotImplementedException("This is just a stub to be removed at build when processed by Fody."); }
-    public static String Of<T>() { throw GetNotImplementedException(); }
-    public static String Of(Object @object) { throw GetNotImplementedException(); }
-    public static String Of<TResult>(Func<TResult> objectOrMethod) { throw GetNotImplementedException(); }
-    public static String Of<T, TResult>(Func<T, Func<TResult>> method) { throw GetNotImplementedException(); }
-    public static String Of<T>(Func<T, Object> fieldOrProperty) { throw GetNotImplementedException(); }
-    public static String Of(Action<EventHandler> @event) { throw GetNotImplementedException(); } 
-    public static String Of<T>(Action<T, EventHandler> @event) { throw GetNotImplementedException(); }
-    public static String OfVoid(Action voidMethod) { throw GetNotImplementedException(); }
-	public static String OfVoid<T>(Func<T, Action> voidMethod) { throw GetNotImplementedException(); }
-	public static String OfStatic<T, TResult>(Func<T, TResult> staticMethod) { throw GetNotImplementedException(); }
-	public static String OfStaticVoid<T>(Action<T> staticVoidMethodWithArgument) { throw GetNotImplementedException(); }
+	private static NotSupportedException GetException() { return new NotSupportedException("This is just a stub to be removed at build when processed by Fody."); }
+    public static String Of<T>() { throw GetException(); }
+    public static String Of(Object @object) { throw GetException(); }
+    public static String Of<TResult>(Func<TResult> objectOrMethod) { throw GetException(); }
+    public static String Of<T, TResult>(Func<T, Func<TResult>> method) { throw GetException(); }
+    public static String Of<T>(Func<T, Object> fieldOrProperty) { throw GetException(); }
+    public static String Of(Action<EventHandler> @event) { throw GetException(); } 
+    public static String Of<T>(Action<T, EventHandler> @event) { throw GetException(); }
+    public static String OfVoid(Action voidMethod) { throw GetException(); }
+	public static String OfVoid<T>(Func<T, Action> voidMethod) { throw GetException(); }
+	public static String OfStatic<T, TResult>(Func<T, TResult> staticMethod) { throw GetException(); }
+	public static String OfStaticVoid<T>(Action<T> staticVoidMethodWithArgument) { throw GetException(); }
 }
