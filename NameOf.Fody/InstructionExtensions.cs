70f7d8bfd6e65c6952eab3b80c28d7af6260dac9
Nick Strupat <nick.strupat@gmail.com>
2015-04-06 17:50:27 -0400

    refactor to cache the `Name.Of` call patterns

--- NameOf.Fody/InstructionExtensions.cs
+++ NameOf.Fody/InstructionExtensions.cs
@@ -1,18 +1,12 @@
 using System.Collections.Generic;
 using Mono.Cecil.Cil;
 
-namespace NameOf.Fody
-{
-    public static class InstructionExtensions
-    {
-        public static IEnumerable<Instruction> AsReverseEnumerable(this Instruction instruction)
-        {
-            yield return instruction;
-
-            while (instruction.Previous != null)
-            {
-                yield return instruction = instruction.Previous;
-            }
-        }
-    }
+namespace NameOf.Fody {
+	public static class InstructionExtensions {
+		public static IEnumerable<Instruction> AsReverseEnumerable(this Instruction instruction) {
+			yield return instruction;
+			while (instruction.Previous != null)
+				yield return instruction = instruction.Previous;
+		}
+	}
 }
